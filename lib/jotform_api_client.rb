4533c2511acdd96d80309789f434adc2e836192e
Donald Wu <yeukfei02@gmail.com>
2022-01-25 18:55:54 +0800

    fix: separate diff test case

--- lib/jotform_api_client.rb
+++ lib/jotform_api_client.rb
@@ -108,6 +108,24 @@ class JotFormApiClient
     response_body
   end
 
+  def get_form_questions(id = '')
+    response = submit_get_request("/form/#{id}/questions")
+
+    response_body = {}
+    response_body = response.body if response.status == 200
+
+    response_body
+  end
+
+  def get_form_question(form_id = '', question_id = '')
+    response = submit_get_request("/form/#{form_id}/question/#{question_id}")
+
+    response_body = {}
+    response_body = response.body if response.status == 200
+
+    response_body
+  end
+
   private
 
   def request_base
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -6,7 +6,7 @@ Dotenv.load
 require 'faker'
 require_relative '../lib/jotform_api_client'
 
-describe 'JotFormApiClient' do
+describe 'user test' do
   let!(:jotform_api_client) do
     api_key = ENV['JOTFORM_API_KEY']
     JotFormApiClient.new(api_key)
@@ -16,18 +16,18 @@ describe 'JotFormApiClient' do
     before(:each) do
       response = jotform_api_client.user
 
-      @@response_json = JSON.parse(response)
-      puts "@response_json = #{@@response_json}"
+      @response_json = JSON.parse(response)
+      puts "@response_json = #{@response_json}"
     end
 
     it 'returns success' do
-      response_code = @@response_json['responseCode']
+      response_code = @response_json['responseCode']
       expect(response_code).to eq(200)
 
-      message = @@response_json['message']
+      message = @response_json['message']
       expect(message).to eq('success')
 
-      content = @@response_json['content']
+      content = @response_json['content']
       expect(!content.nil? && !content.empty?).to be_truthy
     end
   end
@@ -191,52 +191,4 @@ describe 'JotFormApiClient' do
       expect(!content.nil? && !content.empty?).to be_truthy
     end
   end
-
-  # context 'create form test' do
-  #   before(:each) do
-  #     params = {
-  #       'questions[1][type]': Faker::Lorem.word,
-  #       'questions[1][text]': Faker::Lorem.word,
-  #       'questions[1][order]': Faker::Lorem.word,
-  #       'questions[1][name]': Faker::Lorem.word,
-  #       'properties[title]': Faker::Lorem.word
-  #     }
-  #     response = jotform_api_client.create_form(params)
-
-  #     @response_json = JSON.parse(response)
-  #     puts "@response_json = #{@response_json}"
-  #   end
-
-  #   it 'returns success' do
-  #     response_code = @response_json['responseCode']
-  #     expect(response_code).to eq(200)
-
-  #     message = @response_json['message']
-  #     expect(message).to eq('success')
-
-  #     content = @response_json['content']
-  #     expect(!content.nil? && !content.empty?).to be_truthy
-  #   end
-  # end
-
-  # context 'get form by id test' do
-  #   before(:each) do
-  #     id = '30693660548966'
-  #     response = jotform_api_client.get_form_by_id(id)
-
-  #     @response_json = JSON.parse(response)
-  #     puts "@response_json = #{@response_json}"
-  #   end
-
-  #   it 'returns success' do
-  #     response_code = @response_json['responseCode']
-  #     expect(response_code).to eq(200)
-
-  #     message = @response_json['message']
-  #     expect(message).to eq('success')
-
-  #     content = @response_json['content']
-  #     expect(!content.nil? && !content.empty?).to be_truthy
-  #   end
-  # end
 end
