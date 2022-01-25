582e5171c9b7df6bb57637c9f6fa66f958db3650
Donald Wu <yeukfei02@gmail.com>
2022-01-25 11:56:28 +0800

    feat: add private func

--- lib/jotform_api_client.rb
+++ lib/jotform_api_client.rb
@@ -10,13 +10,7 @@ class JotFormApiClient
   end
 
   def user
-    conn = Faraday.new(
-      url: @root_url,
-      headers: { 'Content-Type': 'application/json' }
-    )
-    response = conn.get('/user') do |req|
-      req.params['apiKey'] = @api_key
-    end
+    response = submit_get_request('/user')
 
     response_body = {}
     response_body = response.body if response.status == 200
@@ -25,13 +19,7 @@ class JotFormApiClient
   end
 
   def user_usage
-    conn = Faraday.new(
-      url: @root_url,
-      headers: { 'Content-Type': 'application/json' }
-    )
-    response = conn.get('/user/usage') do |req|
-      req.params['apiKey'] = @api_key
-    end
+    response = submit_get_request('/user/usage')
 
     response_body = {}
     response_body = response.body if response.status == 200
@@ -40,13 +28,7 @@ class JotFormApiClient
   end
 
   def user_submissions
-    conn = Faraday.new(
-      url: @root_url,
-      headers: { 'Content-Type': 'application/json' }
-    )
-    response = conn.get('/user/submissions') do |req|
-      req.params['apiKey'] = @api_key
-    end
+    response = submit_get_request('/user/submissions')
 
     response_body = {}
     response_body = response.body if response.status == 200
@@ -55,13 +37,7 @@ class JotFormApiClient
   end
 
   def user_subusers
-    conn = Faraday.new(
-      url: @root_url,
-      headers: { 'Content-Type': 'application/json' }
-    )
-    response = conn.get('/user/subusers') do |req|
-      req.params['apiKey'] = @api_key
-    end
+    response = submit_get_request('/user/subusers')
 
     response_body = {}
     response_body = response.body if response.status == 200
@@ -70,13 +46,7 @@ class JotFormApiClient
   end
 
   def user_folders
-    conn = Faraday.new(
-      url: @root_url,
-      headers: { 'Content-Type': 'application/json' }
-    )
-    response = conn.get('/user/folders') do |req|
-      req.params['apiKey'] = @api_key
-    end
+    response = submit_get_request('/user/folders')
 
     response_body = {}
     response_body = response.body if response.status == 200
@@ -85,13 +55,7 @@ class JotFormApiClient
   end
 
   def user_reports
-    conn = Faraday.new(
-      url: @root_url,
-      headers: { 'Content-Type': 'application/json' }
-    )
-    response = conn.get('/user/reports') do |req|
-      req.params['apiKey'] = @api_key
-    end
+    response = submit_get_request('/user/reports')
 
     response_body = {}
     response_body = response.body if response.status == 200
@@ -100,13 +64,7 @@ class JotFormApiClient
   end
 
   def user_settings
-    conn = Faraday.new(
-      url: @root_url,
-      headers: { 'Content-Type': 'application/json' }
-    )
-    response = conn.get('/user/settings') do |req|
-      req.params['apiKey'] = @api_key
-    end
+    response = submit_get_request('/user/settings')
 
     response_body = {}
     response_body = response.body if response.status == 200
@@ -115,13 +73,7 @@ class JotFormApiClient
   end
 
   def user_history
-    conn = Faraday.new(
-      url: @root_url,
-      headers: { 'Content-Type': 'application/json' }
-    )
-    response = conn.get('/user/history') do |req|
-      req.params['apiKey'] = @api_key
-    end
+    response = submit_get_request('/user/history')
 
     response_body = {}
     response_body = response.body if response.status == 200
@@ -130,17 +82,23 @@ class JotFormApiClient
   end
 
   def user_forms
-    conn = Faraday.new(
-      url: @root_url,
-      headers: { 'Content-Type': 'application/json' }
-    )
-    response = conn.get('/user/forms') do |req|
-      req.params['apiKey'] = @api_key
-    end
+    response = submit_get_request('/user/forms')
 
     response_body = {}
     response_body = response.body if response.status == 200
 
     response_body
+  end
+
+  private
+
+  def submit_get_request(url)
+    conn = Faraday.new(
+      url: @root_url,
+      headers: { 'Content-Type': 'application/json' }
+    )
+    conn.get(url) do |req|
+      req.params['apiKey'] = @api_key
+    end
   end
 end
